b9f421088e56d772c18d2e47fa7eb151e3573b81
Sierra Wallace <sskwallace@gmail.com>
2020-12-21 03:55:54 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
