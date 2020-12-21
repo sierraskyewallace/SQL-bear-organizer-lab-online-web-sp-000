c7c0ae105321e01ad64610b1de13c77d0fbfa88b
Sierra Wallace <sskwallace@gmail.com>
2020-12-21 04:00:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 10, "Male", "Brown", "Good", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 10, "Male", "Brown", "Good", 1);
 
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "Male", "Black", "Bad", 0);
 
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 15, "Female", "Black", "Good", 1);
 
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", )
 
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
